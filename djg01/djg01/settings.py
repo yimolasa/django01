304893c3bd14122354cae89c23d4cf5fae7ab4d5
Qi <Edward.Qi@sig.biz>
2020-04-14 12:40:27 +0800

    bs initial

--- djg01/djg01/settings.py
+++ djg01/djg01/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django_python3_ldap',
     'polls.apps.PollsConfig',
     'denglu.apps.DengluConfig',
+    'bootstrap4',
     # 'denglu2.apps.Denglu2Config',
     # 'psql01.apps.Psql01Config',
     'bootstrap01.apps.Bootstrap01Config',
@@ -140,7 +141,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 STATIC_URL = '/static/'
 
 # LOGIN_REDIRECT_URL = '/' # for Denglu2
